07a7fca4255157f364b365f6e99acac5f67c1281
Jesus Chavez <jachavez06@gmail.com>
2017-07-06 09:53:59 -0500

    Implement the UITextFieldDelegate protocol method textFieldDidEndEditing(_:).

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -33,6 +33,9 @@ class ViewController: UIViewController, UITextFieldDelegate{
         return true
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
