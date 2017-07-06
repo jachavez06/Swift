04864e0488d21c03bad1274dac974696ddf2cdb6
Jesus Chavez <jachavez06@gmail.com>
2017-07-05 19:08:56 -0500

    Elements added to UI.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -6,18 +6,15 @@
 //  Copyright © 2017 Testing. All rights reserved.
 //
 
+// This file defines a custom subclass of UIViewController named ViewController. Right now, this class simply inherits all the behavior defined by UIViewController. To override or extend that behavior, you override the methods defined on UIViewController.
 import UIKit
 
 class ViewController: UIViewController {
-
+// As you can see in the ViewController.swift file, the template’s implementation overrides both the viewDidLoad() and didReceiveMemoryWarning() methods; however, the template’s stub implementation doesn’t do anything yet, except call the UIViewController version of these methods. You can add your own code to customize the view controller’s response to these events.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
