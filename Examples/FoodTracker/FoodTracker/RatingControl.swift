6df6552a3454a76e8c2b0db33be43b175f85376e
Jesus Chavez <jachavez06@gmail.com>
2017-07-09 15:11:17 -0500

    Create a subclass of UIStackView.

---
+++ Examples/FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jesus Chavez on 7/9/17.
+//  Copyright © 2017 Testing. All rights reserved.
+//
+
+import UIKit
+
+// Custom view subclass of UIView.
+class RatingControl: UIStackView {
+
+
+}
