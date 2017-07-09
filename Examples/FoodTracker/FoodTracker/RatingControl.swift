4ace1e99e725f448ab82a449d1f2b27cbf795368
Jesus Chavez <jachavez06@gmail.com>
2017-07-09 15:42:05 -0500

    Add an action to the button programmatically.

--- Examples/FoodTracker/FoodTracker/RatingControl.swift
+++ Examples/FoodTracker/FoodTracker/RatingControl.swift
@@ -23,6 +23,11 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton){
+        print("Button pressed.")
+    }
+    
     //MARK: Private Methods
     private func setupButtons(){
         
@@ -35,7 +40,11 @@ class RatingControl: UIStackView {
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
         
-        // Add the button to the stack.
+        // Setup the button action.
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        // The #selector expression returns the Selector value for the provided method. A selector is an opaque value that identifies the method. Older APIs used selectors to dynamically invoke methods at runtime. While newer APIs have largely replaced selectors with blocks, many methods—like performSelector(_:) and addTarget(_:action:forControlEvents:)—still take selectors as arguments. In this example, the #selector(RatingControl.ratingButtonTapped(_:)) expression returns the selector for your ratingButtonTapped(_:) action method. This lets the system call your action method when the button is tapped.
+        
+        // Add the button to the stack (managed by RatingControl).
         addArrangedSubview(button)
     }
 }
