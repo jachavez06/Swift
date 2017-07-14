8d7a1ea6bf2e21db34b95e4438fe414b803bca5a
Jesus Chavez <jachavez06@gmail.com>
2017-07-13 20:53:43 -0500

    Cleanup - remove unused elements.

--- Examples/FoodTracker/FoodTracker/ViewController.swift
+++ Examples/FoodTracker/FoodTracker/ViewController.swift
@@ -59,9 +59,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         
         // Hide the keyboard.
