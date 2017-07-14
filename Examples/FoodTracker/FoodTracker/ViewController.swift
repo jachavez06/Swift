842094bffde847b3fac96db1da43c14f28cbd124
Jesus Chavez <jachavez06@gmail.com>
2017-07-13 20:49:48 -0500

    Connect a rating control outlet to ViewController.swift

--- Examples/FoodTracker/FoodTracker/ViewController.swift
+++ Examples/FoodTracker/FoodTracker/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField! // stores a reference to the text field and configures the storyboard to set up that connection.
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     // The IBOutlet attribute tells Xcode that you can connect to the nameTextField property from Interface Builder (which is why the attribute has the IB prefix).
     // The weak keyword indicates that the reference does not prevent the system from deallocating the referenced object. Weak references help prevent reference cycles; however, to keep the object alive and in memory you need to make sure some other part of your app has a strong reference to the object. In this case, it’s the text field’s superview. A superview maintains a strong reference to all of its subviews. As long as the superview remains alive and in memory, all of the subviews remain alive as well. Similarly, the view controller has a strong reference to its content view—keeping the entire view hierarchy alive and in memory.
