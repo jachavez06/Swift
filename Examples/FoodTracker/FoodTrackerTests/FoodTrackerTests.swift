8ffa5979cc9047cfb32d2788c45225f9b9aba513
Jesus Chavez <jachavez06@gmail.com>
2017-07-16 19:50:22 -0500

    Test no rating and highest rating.

--- Examples/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
+++ Examples/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -17,5 +17,12 @@ class FoodTrackerTests: XCTestCase {
     // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
     func testMealInitializationSucceeds() {
         
+        // Zero rating.
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating.
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
     }
 }
