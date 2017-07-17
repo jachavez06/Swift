bf46606fd230fb2bc1cc4e6ed55973c00fee0170
Jesus Chavez <jachavez06@gmail.com>
2017-07-16 19:54:58 -0500

    Add method for test cases that should fail.

--- Examples/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
+++ Examples/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -14,7 +14,7 @@ class FoodTrackerTests: XCTestCase {
 
     //MARK: Meal Class Tests
     
-    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    // Test Case: Confirm that the Meal initializer returns a Meal object when passed valid parameters.
     func testMealInitializationSucceeds() {
         
         // Zero rating.
@@ -24,5 +24,10 @@ class FoodTrackerTests: XCTestCase {
         // Highest positive rating.
         let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
         XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    // Test Case: Confirm that the Meal initializer returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails(){
+        
     }
 }
